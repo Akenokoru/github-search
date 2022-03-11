de7dd47e0b87c098cd174fb233789fed2621eff3
Akenokoru <akenokoru.pan@gmail.com>
2022-03-11 18:50:24 +0800

    feat: add basic layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+
+        <main className="mx-auto px-3 pb-12">Content</main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
